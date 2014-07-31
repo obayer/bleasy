5d671c8c1d5119997407ffc3467489e753f1e99e
Matthias Schaff <mschaff@inovex.de>
2014-07-31 17:38:16 +0200

    merged power savings and state machine

--- bleazy/bleazy.ino
+++ bleazy/bleazy.ino
@@ -1,111 +1,269 @@
+
+#define POWERPIN 6
+#define RXPIN 8
+#define TXPIN 7
+
 #include <avr/wdt.h>
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
+
 #include <util/delay.h>
+#include <SoftwareSerial.h>
+#include <math.h>
 
-#define LEDPIN 13
+#define INPUTBUFFERSIZE 32
 
-// ISR (TIMER1_COMPA_vect) {
-//   digitalWrite(LEDPIN, !digitalRead(LEDPIN));
-// }
+#define STATE_WAIT 0
+#define STATE_WAITOK 1
+#define STATE_WAITCONN 2
+#define STATE_CONNECTED 3
+#define STATE_SLEEP 4
+#define STATE_WAITSLEEP 5
+#define STATE_WAKE 6
+#define STATE_WAITWAKE 7
 
-// void enable_timer() {
-//   cli();
 
-//   // Reset timer and control register
-//   TCCR1A = 0;     
-//   TCCR1B = 0;     
+//		  RX, TX
+SoftwareSerial softSerial(RXPIN, TXPIN);
 
-//   // Set compare register
-//   OCR1A = 0xFFFF;
+char InputBuffer[INPUTBUFFERSIZE];
+int buffPos = 0;
+int sendCount = 0;
+int state = STATE_WAIT;
 
-//   // Set clear timer on compare
-//   TCCR1B |= (1 << WGM12);
-//   // Prescaler 1024
-//   TCCR1B |= (1 << CS10) | (1 << CS12);
-//   // Set output compare A interrupt
-//   TIMSK1 |= (1 << OCIE1A); 
-  
-//   sei();
-// }
-
-// void enable_watchdog() {
-//   cli();
-
-//   // Reset watchdog status register flag 
-//   MCUSR &= ~(1 << WDRF);
-  
-//   // Must be set to change prescaler
-//   WDTCSR = (1 << WDE) | (1 << WDCE);
-//   // Set prescale to 8 sec.
-//   WDTCSR = (1 << WDP0) | (1 << WDP3);
-//   // Interrupt enable
-//   WDTCSR |= (1 << WDIE);
-  
-//   sei();
-// }
 
 // Watchdog interrupt routine
 ISR (WDT_vect) {
-  wdt_disable();
+	wdt_disable();
 }
 
 void enable_watchdog() {
-  cli();
+	cli();
 
-  wdt_enable(WDTO_500MS);
-  // WDT interrupt enable
-  WDTCSR |= (1 << WDIE);
-  
-  sei();
+	wdt_enable(WDTO_8S);
+	// WDT interrupt enable
+	WDTCSR |= (1 << WDIE);
+
+	sei();
 }
 
 void power_down() {
-  set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-  cli();
+	set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+	cli();
 
-  sleep_enable();
-  sei();
+	sleep_enable();
+	sei();
 
-  sleep_cpu();
-  sleep_disable();
+	sleep_cpu();
+	sleep_disable();
 }
 
 void disable_peripheral() {
-  power_usart0_disable();
-  power_spi_disable();
-  power_twi_disable();
+	power_usart0_disable();
+	power_spi_disable();
+	power_twi_disable();
 
-  power_timer0_disable();
-  power_timer1_disable();
-  power_timer2_disable();
+	power_timer0_disable();
+	power_timer1_disable();
+	power_timer2_disable();
 
-  // Disable all digital input buffers on ADC pins
-  DIDR0 = 0b00111111;
+	// Disable all digital input buffers on ADC pins
+	DIDR0 = 0b00111111;
 }
 
 void prescale_cpu() {
-  // Enable clock prescale changes
-  CLKPR = (1 << CLKPCE);
+	// Enable clock prescale changes
+	CLKPR = (1 << CLKPCE);
 
-  // Prescale by 16 --> avr runs at 1MHz
-  CLKPR = (1 << CLKPS2);
+	// Prescale by 16 --> avr runs at 1MHz
+	CLKPR = (1 << CLKPS2);
 }
 
-// ################################### //
-// ############ Arduino ############## //
-// ################################### //
-void setup() {
-  pinMode(13, OUTPUT);
 
-  disable_peripheral();
-  enable_watchdog();
-  prescale_cpu();
+
+void confCmd(char* cmd){
+	softSerial.print(cmd);
+	delay(100);
 }
 
-void loop() {
-  power_down();
-  digitalWrite(LEDPIN, !digitalRead(LEDPIN));
-  enable_watchdog();
+void setup(){
+	//prescale_cpu();
+	Serial.begin(9600);
+	pinMode(POWERPIN, OUTPUT);
+	pinMode(13, OUTPUT);
+	digitalWrite(13, HIGH);
+	softSerial.begin(9600);
+	powerOnHM10();
+	setupHM10();
+
+	//disable timer
+	//        power_timer0_disable();
+	//        power_timer1_disable();
+	//        power_timer2_disable();
+
+	//watchdog on
+	enable_watchdog();
+	Serial.print("Start configuration");
+
 }
+
+void setupHM10(){
+	//check ack
+	confCmd("AT");
+	//setname
+	confCmd("AT+NAMEBLEASY01");
+	//AT+PWRM 1 autosleep
+
+	// temperatur
+	//	//set service to health thermomenter:
+	//	softSerial.print("AT+UUID0x1809");
+	//	delay(100);
+	//	//set characteristic to temperature
+	//	softSerial.print("AT+CHAR0x2A1C");
+
+	////heatrate
+	confCmd("AT+UUID0x180D");
+	confCmd("AT+CHAR0x2A37");
+
+	//set rate to 1 minute
+	confCmd("AT+RAT01");
+	// connection timeout
+	confCmd("AT+TCON000000");
+
+	//notify on connecttion
+	confCmd("AT+NOTI1");
+	clearBuf(InputBuffer);
+	state=STATE_WAITCONN;
+	Serial.print("\nfinisted setup\n");
+}
+
+void clearBuf(char* buf){
+	int i;
+	for(i=0; i<INPUTBUFFERSIZE; i++)  {
+		InputBuffer[i]=0;
+	}
+}
+
+void wakeHM10(){
+	int i;
+	for(i=0; i<80; i++){
+		softSerial.print("a"); 
+	}
+}
+
+void powerOffHM10(){
+	delay(100);
+	Serial.print("power off");
+	digitalWrite(POWERPIN, LOW);
+	digitalWrite(RXPIN, LOW);
+	digitalWrite(TXPIN, LOW);
+	delay(100);
+}
+
+void powerOnHM10(){
+	Serial.print("power on");
+	digitalWrite(POWERPIN, HIGH);
+	delay(100);
+}
+
+int parseAnswer(char* answer){
+	int res = strncmp(InputBuffer+buffPos-strlen(answer), answer, strlen(answer));
+	if(res==0){
+		Serial.print("\nACK!");
+		Serial.print(answer);
+		Serial.print(" state: ");
+		Serial.print(state);
+		Serial.print("\n");
+		clearBuf(InputBuffer);
+		buffPos=0;
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+
+void sendPayload(){
+	delay(200);
+	char t = 80.0f+5.0*cos(sendCount++*0.2f);
+	char b = 0;
+	softSerial.write(b);
+	softSerial.print(t);
+	softSerial.write(b);
+	softSerial.write(b);
+	Serial.print("sending value ");
+	Serial.print(t);
+	Serial.print("\n");
+}
+
+void loop(){
+	if(state==STATE_CONNECTED && !softSerial.available()){
+		sendPayload();
+	}
+
+	if (softSerial.available()){
+		char c = softSerial.read();
+		Serial.write(c);
+		InputBuffer[buffPos++]=c;
+		parseAnswer("OK");
+		switch(state){
+		case STATE_WAITCONN:
+			if(parseAnswer("CONN")==1){
+				Serial.print("connected!\n");
+				state = STATE_CONNECTED;
+			}
+			break;
+		case STATE_CONNECTED:
+			if(parseAnswer("LOST")==1){
+				Serial.print("connection lost\n");
+				sendCount=0;
+				softSerial.print("AT+SLEEP");
+				state=STATE_WAITSLEEP;
+				delay(500);
+			}
+			break;
+		case STATE_WAITSLEEP:
+			if(parseAnswer("LOST")==1){
+
+			}
+			if(parseAnswer("SLEEP")==1){
+				Serial.print("modul sleeps\n");
+				sendCount=0;
+				state=STATE_SLEEP;
+				enable_watchdog();
+				digitalWrite(13, LOW);
+				power_down();
+				digitalWrite(13, HIGH);
+				powerOnHM10();
+			}
+			delay(100);
+
+			break;
+		case STATE_SLEEP:
+			if(parseAnswer("WAKE")==1){
+				Serial.print("modul is awake!\n");
+				sendCount=0;
+				state=STATE_WAITCONN;
+			}
+
+		default:
+			break;
+		}
+	}
+	if (Serial.available()){
+		//softSerial.write(Serial.read());
+		char c = Serial.read();
+		if(c=='w'){
+			wakeHM10();
+		}
+		if(c=='o'){
+			powerOffHM10();
+		}
+		if(c=='i'){
+			powerOnHM10();
+		}
+	}
+}
+
